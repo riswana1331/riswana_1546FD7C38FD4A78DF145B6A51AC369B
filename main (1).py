0fafb1731ee7143dec5ec6f653a36b1454f57099
riswana1331 <144415192+riswana1331@users.noreply.github.com>
2023-10-12 11:48:26 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,5 +1,16 @@
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-    print("The year is a leap year!")
-else:
-    print("The year isn't a leap year!")
+class Player:
+  def play(self):
+    print("the player is playing cricket.")
+
+class Batsman(Player):
+  def play(self):
+    print("the batsman is batting.")
+
+class Bowler(Player):
+  def play(self):
+    print("the bowler is bowling.")
+
+batsman=Batsman()
+bowler=Bowler ()
+batsman.play()
+bowler.play()
